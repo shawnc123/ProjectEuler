4a80ea1c48854314a3aa5b7e3189efe6347f7d3e
shawnc123 <crabtres@flip2.engr.oregonstate.edu>
2015-02-25 23:18:29 -0800

    problem 17 done

---
+++ 19.py
@@ -0,0 +1,13 @@
+import datetime
+
+d = datetime.date(1901, 1, 1)
+
+print d
+ct = 0
+while d.year < 2001:
+    print d, d.weekday, d.day
+    if d.weekday() == 6 and d.day == 1:
+	ct += 1
+    d += datetime.timedelta(days=1)
+
+print ct
